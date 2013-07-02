c95c38a036211028f599491d2bd57a7bb01543ad
Leonardo Dapporto <leondap@gmail.com>
2013-07-02 00:00:00 +0000

    version 2.5

--- R/recluster.col.R
+++ R/recluster.col.R
@@ -1,15 +1,24 @@
 recluster.col<-function (mat) 
 {
-	mat[,1]<-(mat[,1]-min(mat[,1]))/(max(mat[,1])-min(mat[,1]))
-	mat[,2]<-(mat[,2]-min(mat[,2]))/(max(mat[,2])-min(mat[,2]))
-	colour<-array(data=0,dim=c(dim(mat)[1],(dim(mat)[2])+3))
-	for (t in 1 : dim(mat)[1]){
-		colour[t,1]<-mat[t,1]
-		colour[t,2]<-mat[t,2]
-		colour[t,3]<-round(mat[t,2]*255)
-		colour[t,4]<-round(mat[t,1]*255)
-		colour[t,5]<-round((1-(mat[t,2]))*255)
-		rownames(colour)<-rownames(mat)
+	mat2<-mat
+	if ((max(mat[, 1])-min(mat[, 1]))<(max(mat[, 2])-min(mat[, 2])))
+	{
+		mat2[,1]<-mat[,2]
+		mat2[,2]<-mat[,1]
+	}
+	mat3<-mat2
+	mat3[, 1] <- (mat2[, 1]-min(mat2[, 1]))/(max(mat2[, 1])-min(mat2[,1]))
+	mat3[, 2] <- (mat2[, 2] - min(mat2[, 2]))/(max(mat2[, 1]) - min(mat2[,1]))
+	border<-(1-max(mat3[, 2]))/2
+	mat3[, 2]<-mat3[, 2]+border
+	colour <- array(data = 0, dim = c(dim(mat3)[1], (dim(mat3)[2]) + 3))
+	for (t in 1:dim(mat3)[1]) {
+		colour[t, 1] <- mat3[t, 1]
+		colour[t, 2] <- mat3[t, 2]
+		colour[t, 3] <- round(mat3[t, 2] * 255)
+		colour[t, 4] <- round(mat3[t, 1] * 255)
+		colour[t, 5] <- round((1 - (mat3[t, 2])) * 255)
+		rownames(colour) <- rownames(mat)
 	}
 	return(colour)
 }
--- R/recluster.expl.diss.R
+++ R/recluster.expl.diss.R
@@ -1,44 +1,47 @@
-recluster.expl.diss <- function(tree,dist){
-	dist<-as.matrix(dist)
-	nclust<-NULL
-	result<-NULL
-	res<-NULL
-	tree <- reorder(tree)
-	mat <- nodeHeights(tree)
-	mat2<-mat[order(mat[,1],mat[,2]),]
-	mat2<-mat2[mat2[,1]+mat2[,2]!=0,]
-	mat2<-mat2[!duplicated(mat2[,1]),]
-	mat2<-mat2[mat2[,1]!=mat2[,2],]
-	matrix<-matrix(data=NA,ncol=nrow(mat2),nrow=length(tree$tip.label))
-	comp<-rownames(dist)
-	for (cl in 1:nrow(mat2)) {
-		res<-treeSlice(tree,mat2[cl,1]+0.000001,trivial=TRUE)
-		sub<-length(res)
-		nclust[cl]<-sub
-		for (subtrees in 1:sub) {
-			tip<-length(res[[subtrees]]$tip.label)
-			for (tp in 1:tip) {
-				pos<-match(res[[subtrees]]$tip.label[tp],comp)
-				matrix[pos,cl]<-subtrees
-				}
-			}
-		}
-	beta<-sum(dist)
-	cluster<-NULL
-for (loops in 1:(nrow(mat2)-1)){
-		ref<-as.numeric(matrix[,loops])
-		betapartial<-0
-		for (row in 1:nrow(dist)){
-			for (col in 1:ncol(dist)){
-			if(ref[row]!=ref[col]){betapartial<-betapartial+dist[row,col]}
-			}
-		}
-		cluster[loops]<-betapartial/beta
-	}
-	rownames(matrix)<-comp
-	result$matrix<-matrix
-	result$expl.div<-cluster
-	result$nclust<-nclust
-	return(result)
+recluster.expl.diss<-function (tree, dist, maxcl=NULL) 
+{
+    dist <- as.matrix(dist)
+    nclust <- NULL
+    result <- NULL
+    res <- NULL
+    tree <- reorder(tree)
+    mat <- nodeHeights(tree)
+    mat2 <- mat[order(mat[, 1], mat[, 2]), ]
+    mat2 <- mat2[mat2[, 1] + mat2[, 2] != 0, ]
+    mat2 <- mat2[!duplicated(mat2[, 1]), ]
+    mat2 <- mat2[mat2[, 1] != mat2[, 2], ]
+    if (!is.null(maxcl)){mat2<-mat2[1:maxcl,]}	
+    matrix <- matrix(data = NA, ncol = nrow(mat2), nrow = length(tree$tip.label))
+    comp <- rownames(dist)
+    for (cl in 1:nrow(mat2)) {
+        res <- treeSlice(tree, mat2[cl, 1] + 1e-06, trivial = TRUE)
+        sub <- length(res)
+        nclust[cl] <- sub
+        for (subtrees in 1:sub) {
+            tip <- length(res[[subtrees]]$tip.label)
+            for (tp in 1:tip) {
+                pos <- match(res[[subtrees]]$tip.label[tp], comp)
+                matrix[pos, cl] <- subtrees
+            }
+        }
+    }
+    beta <- sum(dist)
+    cluster <- NULL
+    for (loops in 1:(nrow(mat2) - 1)) {
+        ref <- as.numeric(matrix[, loops])
+        betapartial <- 0
+        for (row in 1:nrow(dist)) {
+            for (col in 1:ncol(dist)) {
+                if (ref[row] != ref[col]) {
+                  betapartial <- betapartial + dist[row, col]
+                }
+            }
+        }
+        cluster[loops] <- betapartial/beta
+    }
+    rownames(matrix) <- comp
+    result$matrix <- matrix
+    result$expl.div <- cluster
+    result$nclust <- nclust
+    return(result)
 }
-
